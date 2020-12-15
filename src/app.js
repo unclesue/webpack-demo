9d9ab4cbc50217009cdaf199b2700ab242eb5eae
suxi <sx_syj@163.com>
2020-12-15 14:58:46 +0800

    esm module

--- src/app.js
+++ src/app.js
@@ -4,6 +4,3 @@ if (process.env.NODE_ENV === 'development') {
   const { mockXHR } = require('../mock')
   mockXHR()
 }
-
-require('./demo/1-2-14-generator-promise')
-require('./demo/ts/2.ts')
---
+++ src/demo/part-2/ejs.js
@@ -0,0 +1,10 @@
+// 加载函数模块
+// console.log(require)
+// 模块对象
+// console.log(module)
+// 导出对象别名
+// console.log(exports)
+// 当前文件的绝对路径
+console.log(__filename)
+// 当前文件所在目录
+console.log(__dirname)
